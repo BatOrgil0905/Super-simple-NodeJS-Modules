c85927c18015140fbe69cb6912db972921c418e6
BatOrgil.Baatar <72812449+BatOrgil0905@users.noreply.github.com>
2021-10-25 12:18:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const sayHi = require("./person");
+const names = require("./names")
+
+sayHi(names.person1);
+sayHi(names.person2);
+sayHi("Mary");
---
+++ names.js
@@ -0,0 +1,12 @@
+const person1 = "John";
+const person2 = "Tom";
+
+module.exports = { person1, person2 }
+
+
+// You can also declare an object for this file
+// const names = {
+//     person1: "Tom",
+//     person2: "John"
+// }
+// module.exports = names
